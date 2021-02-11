46bf9144f2cd0941c00d7e15c22291a2a8935863
ftshijt <728307998@qq.com>
2021-02-11 02:23:52 -0500

    add librimix_data

--- generate_librimix.sh
+++ generate_librimix.sh
@@ -70,7 +70,7 @@ python_path=python
 # If you wish to rerun this script in the future please comment this line out.
 $python_path scripts/augment_train_noise.py --wham_dir $wham_dir
 
-for n_src in 2 3; do
+for n_src in 2; do
   metadata_dir=metadata/Libri$n_src"Mix"
   $python_path scripts/create_librimix_from_metadata.py --librispeech_dir $librispeech_dir \
     --wham_dir $wham_dir \
